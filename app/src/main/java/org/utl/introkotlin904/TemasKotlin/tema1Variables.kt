9c12b13cb181697ead51b7aaf7fa35ecdf5d288e
IDGS-904-22001033 <alberto.rt@outlook.com>
2025-05-06 19:03:02 -0600

    Proyecto de android subido

---
+++ app/src/main/java/org/utl/introkotlin904/TemasKotlin/tema1Variables.kt
@@ -0,0 +1,36 @@
+package org.utl.introkotlin904.TemasKotlin
+
+fun main(){
+    //variables solo lectura val
+    val a=4
+    //variables mutablles var
+
+
+    var b=8
+    var c=10
+
+
+
+    println(a)
+    println("el valor de b es $b")
+    println("el valor de c + 2 es ${c+2}")
+
+    c= a+2
+    c+=8
+    c-=5
+    c*8
+    c/=2
+    println("el valor de c es $c")
+    /*
+    val num1:int=23
+    var num2:int=12
+
+    num2=6
+
+    var  nombre:String="carlos"
+    var caracter:Char='a'
+
+    var num4:Float=12.5f
+    var num5:Double=12.5
+    */
+}
---
+++ app/src/main/java/org/utl/introkotlin904/TemasKotlin/tema2Colecciones.kt
@@ -0,0 +1,27 @@
+package org.utl.introkotlin904.TemasKotlin
+
+/*
+lista
+Map
+MutableMap
+MutableSet
+SetOf
+MutableSetOf
+
+
+val readOnlyFiguras= listOf("cuadrado","triangulo","circulo")
+println(readOnlyFiguras)
+var mutableFiguras = mutableListOf("cuadrado","triangulo","circulo")
+val readOnlyFiguras= listOf("cuadrado","triangulo","circulo")
+val mutableFiguras:List<String> = figura
+
+val fruntas = setOf("manzana","banana","naranja")
+val frutas= mutableSetOf("manzana","banana","naranja")
+
+val coches= mapOf("uno" to 1,"dos" to 2, "tres" to 3)
+println(coches)
+val coches2= mutableMapOf("uno" to 1,"dos" to 2, "tres" to 3)
+println(coches2)
+
+
+* */
